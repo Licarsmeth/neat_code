3d94a83c27cad716581b5e7ab96e185e4a245760
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-01-03 18:56:00 +0545

    first leetcode, took 2 tries first time got some time limit exceed error

---
+++ array/duplicate.js
@@ -0,0 +1,11 @@
+// 217
+var containsDuplicate = function(nums) {
+    nums.sort((a,b) => a -b );
+    for(let i = 0; i< nums.length; i++){
+        if (nums[i] == nums[i+1]){
+            return true;
+        }
+    }
+    return false;
+ };
+
